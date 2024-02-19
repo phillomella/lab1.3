2979a2dc44a52f0eccaf4245740ef73768baf460
phillomella <121259223+phillomella@users.noreply.github.com>
2024-02-19 20:42:06 +0300

    TextFieldScreen

--- app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
@@ -3,9 +3,23 @@ package com.raywenderlich.android.lab1.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCompositionContext
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -26,6 +40,31 @@ fun TextFieldScreen() {
 }
 
 @Composable
+@Preview
 fun MyTextField() {
-    //TODO add your code here
+   val textValue = remember { mutableStateOf("") }
+    val primaryColor =colorResource (id = R.color.purple_200)
+
+    OutlinedTextField (
+
+        label = {Text(text = stringResource(id = R.string.insert_text))},
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = primaryColor,
+            focusedLabelColor = primaryColor
+        ),
+       keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
+        value = textValue.value,
+        onValueChange =  {
+            textValue.value= it
+    },
+    )
+
+/*
+   TextField(
+        value =textValue.value,
+        onValueChange ={textValue.value =it},
+        label = {}
+)
+*/
+
 }
